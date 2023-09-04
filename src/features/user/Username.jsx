43d92d514809ecee1a06c6c326b05202385b0bc4
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-09-04 20:19:51 +1000

    Tailwindcss - flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Soe</div>;
+  return <div className="hidden text-sm font-semibold md:block">Soe</div>;
 }
 
 export default Username;
