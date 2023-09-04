c028c0cfa240e7d2d49d11278d61c76c5310925f
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-09-04 16:28:16 +1000

    Fetching order

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,4 +1,4 @@
-const API_URL = "https://rrrrreact-fast-pizza-api.onrender.com/api";
+const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
 export async function getMenu() {
   const res = await fetch(`${API_URL}/menu`);
