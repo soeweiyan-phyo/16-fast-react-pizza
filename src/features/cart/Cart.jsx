97bbd84b6f9226067effb13a7c84e134ec7e99a3
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-09-04 22:50:24 +1000

    Reusing styles with components

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
 
 const fakeCart = [
   {
@@ -29,16 +31,13 @@ function Cart() {
 
   return (
     <div>
-      <Link
-        to="/menu"
-        className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
-      >
-        &larr; Back to menu
-      </Link>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
+        <Button to="/order/new">Order pizzas</Button>
+
         <Link to="/order/new">Order pizzas</Link>
         <button>Clear cart</button>
       </div>
